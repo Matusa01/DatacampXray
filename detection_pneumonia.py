29f28547d108aa6f909c35abf569c9bd6e8ecb60
rukshiniRasa <62998600+rukshiniRasa@users.noreply.github.com>
2022-10-24 14:25:53 +0200

    Update detection_pneumonia.py

--- detection_pneumonia.py
+++ detection_pneumonia.py
@@ -149,7 +149,7 @@ if chart_visual == 'About us':
 
     with col1:
         image_ruk = Image.open('ruk.png')
-        st.image(image_ruk,width = 250)
+        st.image(image_ruk,width = 230)
         
     with col2:
         st.write("Lastname : RASAKUMARAN")
