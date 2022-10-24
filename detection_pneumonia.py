79250570a32d17d04b3d3664be3ba2a07ce9269a
rukshiniRasa <62998600+rukshiniRasa@users.noreply.github.com>
2022-10-24 15:43:08 +0200

    Update detection_pneumonia.py

--- detection_pneumonia.py
+++ detection_pneumonia.py
@@ -139,6 +139,27 @@ if chart_visual == 'Detection of pneumonia':
           
 
 if chart_visual == 'About us':
+    def load_lottieurlink(url: str):
+    r = requests.get(url)
+    if r.status_code !=200:
+      return None
+    return r.json()
+
+  lottie_about_us = load_lottieurlink("https://assets2.lottiefiles.com/packages/lf20_v1yudlrx.json%22)
+
+  if chart_visual == 'About us':
+    st.title("Our team:")
+    st_lottie(
+      lottie_about_us,
+      speed = 1,
+      quality = "low",
+      loop = True,
+    )
+
+
+
+    st.balloons()
+  
     st.title('About us')
     st.write('We are 3 students from Efrei Paris. We are currently studying in first year of masters in Business Intelligence and Analytics')
     st.write('During this semester, we got aksed to do a project in Data camp. So we have choosed the pneumonia detection.')
